ae3d5c10db66808a0211b39d6f5cd3cfe1657f1c
ibrahimalsamarrae <106987852+ibrahimalsamarrae@users.noreply.github.com>
2023-03-03 22:49:40 +0300

    Update 2_📚_Projects.py

--- pages/2_📚_Projects.py
+++ pages/2_📚_Projects.py
@@ -135,5 +135,6 @@ components.html(
       integrity="sha384-JEW9xMcG8R+pH31jmWH6WWP0WintQrMb4s7ZOdauHnUtxwoG2vI5DkLtS3qm9Ekf"
       crossorigin="anonymous"></script>
 </body>
-    """
+    """,
+    width=800, height=900, scrolling=True
 )
