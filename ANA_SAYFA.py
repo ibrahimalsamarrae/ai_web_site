a4cc49dd3919daa5d083cdfec08fceaf1fbc36f4
ibrahimalsamarrae <106987852+ibrahimalsamarrae@users.noreply.github.com>
2023-03-04 02:27:24 +0300

    Update ANA_SAYFA.py

--- ANA_SAYFA.py
+++ ANA_SAYFA.py
@@ -50,29 +50,17 @@ text-align: center;
 """
 
 st.markdown(footer, unsafe_allow_html=True)
-import base64
 
-@st.cache(allow_output_mutation=True)
-def get_base64_of_bin_file(bin_file):
-    with open(bin_file, 'rb') as f:
-        data = f.read()
-    return base64.b64encode(data).decode()
+page_bg_img = '''
+<style>
+body {
+background-image: url("https://images.unsplash.com/photo-1542281286-9e0a16bb7366");
+background-size: cover;
+}
+</style>
+'''
 
-def set_png_as_page_bg(png_file):
-    bin_str = get_base64_of_bin_file(png_file)
-    page_bg_img = '''
-    <style>
-    body {
-    background-image: url("data:image/png;base64,%s");
-    background-size: cover;
-    }
-    </style>
-    ''' % bin_str
-    
-    st.markdown(page_bg_img, unsafe_allow_html=True)
-    return
-
-set_png_as_page_bg('2.jpg')
+st.markdown(page_bg_img, unsafe_allow_html=True)
 
 if __name__ == "__main__":
     main()
