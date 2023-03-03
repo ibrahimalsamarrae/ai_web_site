470cb83261b5e7874495123e6db6ef196eda959f
ibrahimalsamarrae <106987852+ibrahimalsamarrae@users.noreply.github.com>
2023-03-04 01:10:03 +0300

    Update ANA_SAYFA.py

--- ANA_SAYFA.py
+++ ANA_SAYFA.py
@@ -4,8 +4,7 @@ import tensorflow as tf
 # im = Image.open("favicon.ico")
 st.set_page_config(
     page_title="ANA SAYFA",
-    page_icon="☔",
-    layout="wide",
+    page_icon=":umbrella_with_rain_drops:,
 )
 
 st.title("AI PROGRAMIZA HOŞ GELDİNİZ💫")
