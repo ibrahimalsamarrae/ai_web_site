5921d3f5275bce319be3aef29fb27cc20c68c1d9
ibrahimalsamarrae <106987852+ibrahimalsamarrae@users.noreply.github.com>
2023-03-04 20:29:43 +0300

    Update 2_📚_Projects.py

--- pages/2_📚_Projects.py
+++ pages/2_📚_Projects.py
@@ -2,12 +2,8 @@ import streamlit as st
 from PIL import Image
 
 st.title("sonuçu yazdır")
-file =st.session_state("my_input")
-age =st.session_state("age")
-if file is not None:
-    st.image(file, width=300)
-    image = Image.open(file)
-    st.markdown("## yaşınız %i gibi görünüyor" %age[0][0])
+
+image = Image.open(st.session_state("my_input")) 
     
        
 #st.write("You have entered", st.session_state["my_input"])
