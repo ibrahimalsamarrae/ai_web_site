4d4328e2cf05c6d812a52579358a628c9c676d32
ibrahimalsamarrae <106987852+ibrahimalsamarrae@users.noreply.github.com>
2023-03-04 22:24:07 +0300

    Update 3_💬_Hakkında.py

--- pages/3_💬_Hakkında.py
+++ pages/3_💬_Hakkında.py
@@ -1,3 +1,31 @@
 import streamlit as st
 
 st.title("Hakkında")
+
+
+
+
+
+
+footer="""<style>
+.footer {
+position: fixed;
+left: 0;
+bottom: 0;
+width: 60%;
+background-color: black;
+color: white;
+text-align: center;
+}
+ #MainMenu {visibility: hidden;}
+</style>
+
+<div class="footer">
+</center><p> by ALSAMARRAE</p><center> 
+</div>
+
+"""
+
+st.markdown(footer, unsafe_allow_html=True)
+
+ 
