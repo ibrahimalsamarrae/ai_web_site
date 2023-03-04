ca4fda305bb61fa98f569968088a41d1e22aa819
ibrahimalsamarrae <106987852+ibrahimalsamarrae@users.noreply.github.com>
2023-03-04 22:30:58 +0300

    Create 2_💬Kamera' ile Resim çek.py

---
+++ pages/2_💬Kamera' ile Resim çek.py
@@ -0,0 +1,40 @@
+import streamlit as st
+
+st.title("Hakkında")
+
+import streamlit as st
+
+img_file_buffer = st.camera_input("Take a picture")
+
+if img_file_buffer is not None:
+    # To read image file buffer as bytes:
+    bytes_data = img_file_buffer.getvalue()
+    # Check the type of bytes_data:
+    # Should output: <class 'bytes'>
+    st.write(type(bytes_data))
+
+
+
+
+footer="""<style>
+.footer {
+position: fixed;
+left: 0;
+bottom: 0;
+width: 60%;
+background-color: black;
+color: white;
+text-align: center;
+}
+ #MainMenu {visibility: hidden;}
+</style>
+
+<div class="footer">
+</center><p> by ALSAMARRAE</p><center> 
+</div>
+
+"""
+
+st.markdown(footer, unsafe_allow_html=True)
+
+ 
