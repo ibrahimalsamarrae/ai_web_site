f3b4c95cddfc07482b373b227d5b8088ee54223b
ibrahimalsamarrae <106987852+ibrahimalsamarrae@users.noreply.github.com>
2023-03-04 23:48:32 +0300

    Update Ana_Sayfa.py

--- Ana_Sayfa.py
+++ Ana_Sayfa.py
@@ -40,55 +40,10 @@ if selected == "kamera'ile çek":
             age = model.predict(image)
             age=st.markdown("## yaşınız %i gibi görünüyor" %age[0][0])
 
-
-    footer="""<style>
-    .footer {
-    position: fixed;
-    left: 0;
-    bottom: 0;
-    width: 60%;
-    background-color: black;
-    color: white;
-    text-align: center;
-    }
-     #MainMenu {visibility: hidden;}
-    </style>
-
-    <div class="footer">
-    </center><p> by ALSAMARRAE</p><center> 
-    </div>
-
-    """
-    st.markdown(footer, unsafe_allow_html=True)
-
    ################################################################
 elif selected == "Hakkında":
     st.title("Hakkında")
     st.header("Yapay zeka tarafından makine öğrenimi kullanılarak oluşturulmuş bir programdır. Model, farklı yaşlardaki 23.000 görüntü örneği kullanılarak eğitildi. Modelin eğitiminde sinir ağlarından en son algoritmalar kullanılmıştır. Program çok güvenlidir ve gizliliğinizi korur")
-
-
-              
-    footer="""<style>
-    .footer {
-    position: fixed;
-    left: 0;
-    bottom: 0;
-    width: 60%;
-    background-color: black;
-    color: white;
-    text-align: center;
-    }
-     #MainMenu {visibility: hidden;}
-    </style>
-
-    <div class="footer">
-    </center><p> by ALSAMARRAE</p><center> 
-    </div>
-
-    """
-
-    st.markdown(footer, unsafe_allow_html=True)
-
 
 
 def main():
