af22b096704cbc0ca0987aea75220e66e51d041e
ibrahimalsamarrae <106987852+ibrahimalsamarrae@users.noreply.github.com>
2023-03-04 22:32:17 +0300

    Update 2_💬Kamera' ile Resim çek.py

--- pages/2_💬Kamera' ile Resim çek.py
+++ pages/2_💬Kamera' ile Resim çek.py
@@ -1,10 +1,10 @@
 import streamlit as st
 
-st.title("Hakkında")
+st.title("kamera' ile resim çek ve sonoç al")
 
 import streamlit as st
 
-img_file_buffer = st.camera_input("Take a picture")
+img_file_buffer = st.camera_input("çek")
 
 if img_file_buffer is not None:
     # To read image file buffer as bytes:
