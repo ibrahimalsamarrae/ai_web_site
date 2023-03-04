9bfd0985fd7d42b511fc0334f2ba0d3ede04dda1
ibrahimalsamarrae <106987852+ibrahimalsamarrae@users.noreply.github.com>
2023-03-04 22:38:21 +0300

    Update 2_💬Kamera' ile Resim çek.py

--- pages/2_💬Kamera' ile Resim çek.py
+++ pages/2_💬Kamera' ile Resim çek.py
@@ -1,19 +1,39 @@
 import streamlit as st
+from PIL import Image
+import tensorflow as tf
+img= Image.open("1.jpg")
+st.set_page_config(
+        page_title="resim çek",
+        page_icon=img
+    )
 
 st.title("kamera' ile resim çek ve sonoç al")
 
-import streamlit as st
+file = st.camera_input("Take a picture")
 
-img_file_buffer = st.camera_input("çek")
-
-if img_file_buffer is not None:
+if file is not None:
     # To read image file buffer as bytes:
-    bytes_data = img_file_buffer.getvalue()
+    bytes_data = file.getvalue()
     # Check the type of bytes_data:
     # Should output: <class 'bytes'>
     st.write(type(bytes_data))
 
-
+submit = st.button("sonuç al")
+       
+      
+if submit:
+    if file is not None:
+        st.image(file, width=300)
+        image = Image.open(file)
+        
+        image = tf.keras.preprocessing.image.img_to_array(image)
+        image = tf.image.resize(image, [224,224]) 
+        image = image / 255.0      
+        image = tf.expand_dims(image, axis=0)
+        
+        model = tf.keras.models.load_model("yasmodel.h5")
+        age = model.predict(image)
+        age=st.markdown("## yaşınız %i gibi görünüyor" %age[0][0])
 
 
 footer="""<style>
@@ -34,7 +54,6 @@ text-align: center;
 </div>
 
 """
-
 st.markdown(footer, unsafe_allow_html=True)
 
  
