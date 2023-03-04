b27a1de7b85548b19714a8420d9ac3f23d9d2ea8
ibrahimalsamarrae <106987852+ibrahimalsamarrae@users.noreply.github.com>
2023-03-04 23:47:04 +0300

    Update Ana_Sayfa.py

--- Ana_Sayfa.py
+++ Ana_Sayfa.py
@@ -1,6 +1,8 @@
 import streamlit as st
 from PIL import Image
 import tensorflow as tf
+from streamlit_option_menu import option_menu
+
 img= Image.open("1.jpg")
 st.set_page_config(
         page_title="ANA SAYFA",
@@ -9,6 +11,84 @@ st.set_page_config(
 
 st.title("AI PROGRAMIZA HOŞ GELDİNİZ💫")
 st.sidebar.success("Yukarıdan bir sayfa seçin.")
+
+
+
+
+selected = option_menu("Menu", ["kamera'ile çek", 'Hakkında'], 
+icons=['house', 'gear'], default_index=0)
+
+if selected == "kamera'ile çek":
+    st.title("kamera' ile resim çek ve sonoç al")
+
+    file = st.camera_input("")
+
+    submit = st.button("sonuç al")
+           
+          
+    if submit:
+        if file is not None:
+            st.image(file, width=300)
+            image = Image.open(file)
+            
+            image = tf.keras.preprocessing.image.img_to_array(image)
+            image = tf.image.resize(image, [224,224]) 
+            image = image / 255.0      
+            image = tf.expand_dims(image, axis=0)
+            
+            model = tf.keras.models.load_model("yasmodel.h5")
+            age = model.predict(image)
+            age=st.markdown("## yaşınız %i gibi görünüyor" %age[0][0])
+
+
+    footer="""<style>
+    .footer {
+    position: fixed;
+    left: 0;
+    bottom: 0;
+    width: 60%;
+    background-color: black;
+    color: white;
+    text-align: center;
+    }
+     #MainMenu {visibility: hidden;}
+    </style>
+
+    <div class="footer">
+    </center><p> by ALSAMARRAE</p><center> 
+    </div>
+
+    """
+    st.markdown(footer, unsafe_allow_html=True)
+
+   ################################################################
+elif selected == "Hakkında":
+    st.title("Hakkında")
+    st.header("Yapay zeka tarafından makine öğrenimi kullanılarak oluşturulmuş bir programdır. Model, farklı yaşlardaki 23.000 görüntü örneği kullanılarak eğitildi. Modelin eğitiminde sinir ağlarından en son algoritmalar kullanılmıştır. Program çok güvenlidir ve gizliliğinizi korur")
+
+
+              
+    footer="""<style>
+    .footer {
+    position: fixed;
+    left: 0;
+    bottom: 0;
+    width: 60%;
+    background-color: black;
+    color: white;
+    text-align: center;
+    }
+     #MainMenu {visibility: hidden;}
+    </style>
+
+    <div class="footer">
+    </center><p> by ALSAMARRAE</p><center> 
+    </div>
+
+    """
+
+    st.markdown(footer, unsafe_allow_html=True)
+
 
 
 def main():
