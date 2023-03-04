08338b77e4820870f926f4f06300f73cfbd2e246
ibrahimalsamarrae <106987852+ibrahimalsamarrae@users.noreply.github.com>
2023-03-04 20:12:52 +0300

    Update 2_📚_Projects.py

--- pages/2_📚_Projects.py
+++ pages/2_📚_Projects.py
@@ -1,15 +1,13 @@
 import streamlit as st
+from PIL import Image
 
-
-st.title("Projects")
-
-
+st.title("sonuçu yazdır")
+file =st.session_state("file")
+age =st.session_state("age")
+if file is not None:
+    st.image(file, width=300)
+    image = Image.open(file)
+    st.markdown("## yaşınız %i gibi görünüyor" %age[0][0])
+    
+       
 st.write("You have entered", st.session_state["my_input"])
-
-hide_streamlit_style = """
-            <style>
-            #MainMenu {visibility: hidden;}
-            footer {visibility: hidden;}
-            </style>
-            """
-st.markdown(hide_streamlit_style, unsafe_allow_html=True) 
