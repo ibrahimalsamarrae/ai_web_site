846eb92cb792d493cf7d643af5510a6b1e8c9428
ibrahimalsamarrae <106987852+ibrahimalsamarrae@users.noreply.github.com>
2023-03-04 20:02:18 +0300

    Update ANA_SAYFA.py

--- ANA_SAYFA.py
+++ ANA_SAYFA.py
@@ -16,19 +16,22 @@ def main():
        st.header("Görünüşüne göre yaşını bil")
        st.write("note: resminizi yüzünüz billi olsun")
        file = st.file_uploader("lütfen geçerli bir fotograf ekleyen",help=None)
-       if file is not None:
-            st.image(file, width=300)
-            image = Image.open(file)
-            
-            image = tf.keras.preprocessing.image.img_to_array(image)
-            image = tf.image.resize(image, [224,224]) 
-            image = image / 255.0      
-            image = tf.expand_dims(image, axis=0)
-            
-            model = tf.keras.models.load_model("yasmodel.h5")
-            age = model.predict(image)
-            
-            st.markdown("## yaşınız %i gibi görünüyor" %age[0][0])
+       submit = st.button("sonuç al")
+       if submit:
+           if file is not None:
+                st.image(file, width=300)
+                image = Image.open(file)
+                
+                image = tf.keras.preprocessing.image.img_to_array(image)
+                image = tf.image.resize(image, [224,224]) 
+                image = image / 255.0      
+                image = tf.expand_dims(image, axis=0)
+                
+                model = tf.keras.models.load_model("yasmodel.h5")
+                age = model.predict(image)
+                
+                st.markdown("## yaşınız %i gibi görünüyor" %age[0][0])
+
             
             
 
