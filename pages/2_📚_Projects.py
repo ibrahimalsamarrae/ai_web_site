a60f8b779dd021b0bb69ac983404c104dc70799d
ibrahimalsamarrae <106987852+ibrahimalsamarrae@users.noreply.github.com>
2023-03-04 21:59:34 +0300

    Delete 2_📚_Projects.py

--- pages/2_📚_Projects.py
+++
@@ -1,9 +0,0 @@
-import streamlit as st
-from PIL import Image
-
-st.title("sonuçu yazdır")
-
-image = Image.open(st.session_state("my_input")) 
-    
-       
-#st.write("You have entered", st.session_state["my_input"])
